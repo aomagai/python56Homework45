3d7840ccf15de7d00c3c03f3d8e675e0ae9d656f
aomagai <aomagai@mail.ru>
2020-07-23 12:47:26 +0600

    Создаль модель, добавил базу данных

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+
+class ToDo(models.Model):
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', null=False, blank=False, verbose_name='Статус')
+    date = models.DateField(null=True, blank=True, verbose_name='Дата')
+
+    def __str__(self):
+        return "{0}. {2} ({1})".format(self.pk, self.status, self.description)
