b506492ce87412a3637804624c5d6a8c15ae4283
aomagai <aomagai@mail.ru>
2020-07-23 15:15:59 +0600

    Добавление записи в БД

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -7,3 +7,17 @@ def index_view(request):
     return render(request, 'index.html', context={
         'ToDo_list': data
     })
+
+def create_view(request):
+    if request.method == "GET":
+        return render(request, 'todo_create.html', context={
+            'status_choices': STATUS_CHOICES
+        })
+    elif request.method == 'POST':
+        status = request.POST.get('status')
+        date = request.POST.get('date')
+        description = request.POST.get('description')
+        print(description)
+        todo = ToDo.objects.create( description=description, status=status, date=date)
+        context = {'todo': todo}
+        return render(request, 'todo_view.html', context)
