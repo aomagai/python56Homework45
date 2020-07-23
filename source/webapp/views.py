3e7b581120ba6806971904d0fce5d7a25e044d4f
aomagai <aomagai@mail.ru>
2020-07-23 13:04:07 +0600

    Создал главную страницу и добавил вывод задач на неё.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from webapp.models import ToDo, STATUS_CHOICES
 
-# Create your views here.
+
+def index_view(request):
+    data = ToDo.objects.all()
+    return render(request, 'index.html', context={
+        'ToDo_list': data
+    })
