f36f4c8ab3ecf599d45d10781340145a435bae3a
aomagai <aomagai@mail.ru>
2020-07-28 10:10:15 +0600

    добавлено поле в модель detaled_description, сделана миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,6 +7,7 @@ class ToDo(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', null=False, blank=False, verbose_name='Статус')
     date = models.DateField(null=True, blank=True, verbose_name='Дата')
+    detailed_description = models.TextField(max_length=5000, null=True, blank=True, verbose_name='Подробное описание')
 
     def __str__(self):
         return "{0}. {2} ({1})".format(self.pk, self.status, self.description)
